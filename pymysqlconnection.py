218349d880405fbde740706740cb8b9f63b8fc79
mprogramcode <45775760+mprogramcode@users.noreply.github.com>
2020-01-20 18:11:19 -0500

    Create pymysqlconnection.py

---
+++ pymysqlconnection.py
@@ -0,0 +1,37 @@
+
+#Creates a connection to a Mysql Database
+def SQLLookupQRData(UserTextInputData):
+
+    try:
+        #Create Connection to the database
+        conn = pymysql.connect(host=r'127.0.0.1', user=r'username', password=r'password', db=r'databasename')
+
+    except Exception as e:
+        print("Exeception occured:{}".format(e))
+        conn.close()
+
+    try:                                 
+        # Create a cursor object
+        cursorObject = conn.cursor()                                     
+
+        # SQL query string
+        sqlQuery = "select QRCODE from qrdatatable where id = " + "'" + UserTextInputData + "'"
+
+        # Execute the sqlQuery
+        cursorObject.execute(sqlQuery)
+
+        #Fetch all the rows
+        rows = cursorObject.fetchall()
+
+        #Remove (,) from str 
+        CleanedStr = str(rows[0])
+        CleanedStr = CleanedStr.replace("('", "")
+        CleanedStr = CleanedStr.replace("',)", "")
+    
+        return CleanedStr
+
+    except Exception as e:
+        print("Exeception occured:{}".format(e))
+
+    finally:
+        conn.close()
